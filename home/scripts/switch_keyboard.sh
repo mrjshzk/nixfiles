30b0e2ab4105ecd9d0cd31905155350373d1d744
mrjshzk <mrjsilva05@gmail.com>
2026-01-03 15:20:57 +0000

    switch keyboard layout

---
+++ home/scripts/switch_keyboard.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+layout=$1 # should be pt or us
+# 1 - pt ; 0 - us | according to their definition order in hyprland config
+declare -A layout_mapping
+layout_mapping["pt"]=1
+layout_mapping["us"]=0
+
+hyprctl switchxkblayout all ${layout_mapping[${layout}]}
