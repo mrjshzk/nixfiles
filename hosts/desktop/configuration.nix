4fbc3c93bb3731e6339642fa6de4220ce8398e90
mrjshzk <mrjsilva05@gmail.com>
2026-01-04 03:03:59 +0000

    ok

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -1,3 +1,22 @@
 { pkgs, config, ... }:
 
-import ../..common/keyboard_config.nix { inherit pkgs; } "us"
+{
+  services.xserver.xkb.layout = "us";
+
+  systemd.user.services.host_specific_boot = {
+    after = [ "network.target" ];
+    wantedBy = [ "default.target" ];
+    description = "Switch keyboard layout for the appropriate machine :)";
+    path = with pkgs; [
+      coreutils
+      libnotify
+      hyprland
+      bash
+    ];
+    serviceConfig = {
+      Type = "oneshot";
+      ExecStartPre = "${pkgs.coreutils}/bin/sleep 2";
+      ExecStart = "${pkgs.bash}/bin/bash %h/scripts/switch_keyboard.sh us";
+    };
+  };
+}
--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -1,3 +1,22 @@
 { pkgs, config, ... }:
 
-import ../../common/keyboard_config.nix { inherit pkgs; } "pt"
+{
+  services.xserver.xkb.layout = "pt";
+
+  systemd.user.services.host_specific_boot = {
+    after = [ "network.target" ];
+    wantedBy = [ "default.target" ];
+    description = "Switch keyboard layout for the appropriate machine :)";
+    path = with pkgs; [
+      coreutils
+      libnotify
+      hyprland
+      bash
+    ];
+    serviceConfig = {
+      Type = "oneshot";
+      ExecStartPre = "${pkgs.coreutils}/bin/sleep 2";
+      ExecStart = "${pkgs.bash}/bin/bash %h/scripts/switch_keyboard.sh pt";
+    };
+  };
+}
