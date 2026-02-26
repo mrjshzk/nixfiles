44563e95a32217aa864db2ec625a0e3233905b3f
mrjshzk <mrjsilva05@gmail.com>
2026-02-26 18:48:38 +0000

    remove GTK_IM_MODULE (TODO), fix kde portal pkg

--- modules/window-manager/kde.nix
+++ modules/window-manager/kde.nix
@@ -10,13 +10,14 @@ with lib; {
     # KDE PLASMA 6
     # ============================================
     services.desktopManager.plasma6.enable = true;
+    programs.ssh.askPassword = pkgs.lib.mkForce "${pkgs.kdePackages.ksshaskpass.out}/bin/ksshaskpass";
 
     # ============================================
     # XDG PORTAL
     # ============================================
     xdg.portal = {
       enable = mkDefault true;
-      extraPortals = [pkgs.xdg-desktop-portal-kde];
+      extraPortals = [pkgs.kdePackages.xdg-desktop-portal-kde];
     };
   };
 }
