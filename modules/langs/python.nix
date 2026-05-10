9d10ca74df4036c0ebc5bce4b8d962f105556221
mrjshzk <mrjsilva05@gmail.com>
2026-05-10 02:57:10 +0100

    add matplotlib

--- modules/langs/python.nix
+++ modules/langs/python.nix
@@ -1,6 +1,7 @@
 {pkgs, ...}: {
   environment.systemPackages = with pkgs; [
     python313Packages.python-lsp-server
+    python313Packages.matplotlib
     python313
     python313Packages.pip
     python313Packages.black
