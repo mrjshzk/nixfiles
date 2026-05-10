ccb86bfe886a03fb5aa3f3cd928fcb06bbca4c8f
mrjshzk <mrjsilva05@gmail.com>
2026-05-10 03:34:15 +0100

    add numpy

--- modules/langs/python.nix
+++ modules/langs/python.nix
@@ -2,6 +2,7 @@
   environment.systemPackages = with pkgs; [
     python313Packages.python-lsp-server
     python313Packages.matplotlib
+    python313Packages.numpy
     python313
     python313Packages.pip
     python313Packages.black
